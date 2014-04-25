615a4874ad5ce0a90c492fe36cbe415afb46c2cd
artfoundry <dave@artfoundry.com>
2014-04-25 00:21:53 -0700

    Adding jQuery Mobile for responsive layout
    * Changing all styling to use jQuery Mobile grids
    * Set up grid for buttons
    * Set up pages for buttons and content

--- js/artfoundry.js
+++ js/artfoundry.js
@@ -1,19 +1,19 @@
-$(window).scroll(function(e){
-  parallax();
-});
+// $(window).scroll(function(e){
+//   parallax();
+// });
 
 function run_animation() {
-  $("#logo-main").fadeIn(1000, function() {
-    $("#logo-main").animate({width: "-=800", height: "-=404", marginLeft: "23%", marginTop: "130px"}, 800, function() {
-      $(".buttons").fadeIn(500);
+  $("#logo-main").css("margin-right", "-600px").fadeIn(1000, function() {
+    $("#logo-main").animate({width: "-=800", height: "-=404", marginRight: "0", marginTop: "130px"}, 800, function() {
+      $("#img-games, #img-art, #img-webapps").fadeIn(500);
     })
   })
 }
 
-function parallax() {
-  var scrolled = $(window).scrollLeft();
-  $('#bg').css('left',-(scrolled*0.2)+'px');
-  $('#mg1').css('left',-(scrolled*0.6)+'px');
-  $('#mg2').css('left',-((scrolled*0.4) - 1000)+'px');
-  $('#fg1').css('left',-((scrolled*1.2) - 400)+'px');
-} 
+// function parallax() {
+//   var scrolled = $(window).scrollLeft();
+//   $('#bg').css('left',-(scrolled*0.2)+'px');
+//   $('#mg1').css('left',-(scrolled*0.6)+'px');
+//   $('#mg2').css('left',-((scrolled*0.4) - 1000)+'px');
+//   $('#fg1').css('left',-((scrolled*1.2) - 400)+'px');
+// } 
